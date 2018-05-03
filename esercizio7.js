7128a407ff9b7ba28f2302d82fa5e87d08cf10af
michelagrasselli <michelagrasselli@outlook.it>
2018-05-03 15:35:06 +0200

    Esercizio 7 risolto

---
+++ esercizio7.js
@@ -0,0 +1,15 @@
+function FunctionMouseover() {
+    window.alert("Errore Mouseover")
+}
+
+function FunctionMouseout() {
+    window.alert("Errore Mouseout")
+}
+
+function FunctionClick() {
+    window.alert("Errore Click")
+}
+
+function FunctionContextmenu() {
+    window.alert("Errore Contextmenu")
+}
